7e51f2bda57bb95b47a563788076f04dbf932689
crvernon <chrisrvernon@gmail.com>
2021-12-14 15:13:37 -0500

    quiet gcamwrapper until install worked out

--- echo/worker.py
+++ echo/worker.py
@@ -1,4 +1,5 @@
-import gcamwrapper as gw
+# TODO:  see comment in function
+# import gcamwrapper as gw
 
 import echo.fetch as fetch
 
@@ -7,16 +8,17 @@ def gcam_worker(slurm_idx, data_dir):
     """Fake model
 
     """
+    # TODO:  build gcamwrapper worker but need to be able to install remotely on actions first
 
-    gcam = gw.Gcam("configuration_ref.xml", "/people/pralitp/test-framework/exe")
-    gcam.run_to_period(1)
-    query = gw.get_query("land", "land_allocation")
-    ret = gcam.get_data(query, {"region": ['=', 'USA'], "year": ['<=', '1990']})
+    # gcam = gw.Gcam("configuration_ref.xml", "/people/pralitp/test-framework/exe")
+    # gcam.run_to_period(1)
+    # query = gw.get_query("land", "land_allocation")
+    # ret = gcam.get_data(query, {"region": ['=', 'USA'], "year": ['<=', '1990']})
+    #
+    # # set would happen here
+    # setter_data = fetch.get_data(slurm_idx, data_dir)
+    # print(setter_data)
+    #
+    # del gcam
 
-    # set would happen here
-    setter_data = fetch.get_data(slurm_idx, data_dir)
-    print(setter_data)
-
-    del gcam
-
-    return ret.head()
+    return None
